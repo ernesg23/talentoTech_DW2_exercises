a2a4bccc343d6b7620cdde92fee33152b4c62cde
Ernesto Manuel Gallego <ernestosecu23@gmail.com>
2025-05-04 23:00:21 -0300

    exercise 2 resolution

--- desafío_clase_2/app.js
+++ desafío_clase_2/app.js
@@ -7,3 +7,8 @@
 // Luego calcular el costo total de la compra multiplicando el precioUnitario por la cantidadDeseada.
 // utiliza alert() para informar al usuario sobre el costo total de su compra.
 
+let nombreProducto="Pelota Messi", precioUnitario=5000, cantidadDeseada;
+
+cantidadDeseada = parseInt(prompt(`¿Qué cantidad desea comprar de nuestro producto llamado '${nombreProducto}'?`))
+
+alert(`El costo total de la compra es: ${precioUnitario*cantidadDeseada}, siendo el precio unitario ${precioUnitario}`)
