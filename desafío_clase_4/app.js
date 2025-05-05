f52ca576c606748f179fa983e8f7f28d65557c11
Ernesto Manuel Gallego <ernestosecu23@gmail.com>
2025-05-04 23:06:52 -0300

    exercise 4 resolution

--- desafío_clase_4/app.js
+++ desafío_clase_4/app.js
@@ -1,2 +1,12 @@
 // Crea una función llamada sumarProductos() que acepte dos parámetros: precioUnitario y cantidadDeseada. CC
 // La función debe calcular y devolver el total gastado en un producto multiplicando el precio por la cantidad comprada.
+let nombreProducto="Pelota Messi", precio=5000, cantidad;
+
+function sumarProductos(precioUnitario, cantidadDeseada)
+{
+    return precioUnitario*cantidadDeseada
+}
+
+cantidad = parseInt(prompt(`¿Qué cantidad desea comprar de nuestro producto llamado '${nombreProducto}'?`))
+
+alert(`El costo total de la compra es: ${sumarProductos(precio,cantidad)}, siendo el precio unitario ${precio}`)
